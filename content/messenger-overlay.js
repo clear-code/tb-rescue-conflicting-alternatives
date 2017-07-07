920411ca9ead6b73d90aa82c37263bee9b7df5a7
YUKI Hiroshi <shimoda@clear-code.com>
2017-07-07 19:51:19 +0900

    Reconstruct based on Promise

--- content/messenger-overlay.js
+++ content/messenger-overlay.js
@@ -4,8 +4,113 @@
   const Cu = Components.utils;
   const Cr = Components.results;
   const Prefs = Cc['@mozilla.org/preferences;1'].getService(Ci.nsIPrefBranch);
+  const { Promise } = Cu.import('resource://gre/modules/Promise.jsm', {});
 
   var ShowFirstBodyPart = {
+    tryUpdateCurrentMessage : function() {
+      var msguri = gFolderDisplay.selectedMessageUris[0];
+      var context = {};
+      var loader = new StreamMessageLoader(context);
+      loader.load(msguri)
+        .then((aContext) => {
+          var message = aContext.message;
+          message = this.prepareMessage(message, aContext.hdr.Charset);
+          message = this.cleanupMozHeaders(message);
+          message = this.markAsApplied(message);
+          message = this.incrementDate(message, aContext.folder);
+
+          var file = this.saveToTempFile(message);
+          var replacer = new MessageReplacer(aContext);
+          return replacer.replaceFromFile(file);
+        })
+        .then((aContext) => {
+          this.restoreState(aContext);
+        });
+    },
+
+    prepareMessage : function(aMessage, aCharset) {
+      var converter = Cc['@mozilla.org/intl/scriptableunicodeconverter'].createInstance(Ci.nsIScriptableUnicodeConverter);
+      var message = aMessage;
+      if (aCharset) {
+        converter.charset = aCharset;
+        // hdr.Charset will not work with multipart messages, so we must try to extract the charset manually
+      }
+      else {
+        try {
+          let messageCut = aMessage.substring(aMessage.indexOf('charset=')+8, aMessage.indexOf('charset=')+35);
+          let mailCharset = messageCut.match(/[^\s]+/).toString();
+          mailCharset = mailCharset.replace(/\"/g, '');
+          mailCharset = mailCharset.replace(/\'/g, '');
+          converter.charset = mailCharset;
+        }
+        catch(e) {
+          converter.charset = 'UTF-8';
+        }
+      }
+      try {
+        aMessage = converter.ConvertToUnicode(aMessage);
+      }
+      catch(e) {}
+      aMessage = this.cleanCRLF(aMessage);
+      try {
+        aMessage = converter.ConvertFromUnicode(aMessage);
+      }
+      catch(e) {}
+      return aMessage;
+    },
+
+    cleanCRLF : function(data) {
+      /* This function forces all newline as CRLF; this is useful for some reasons
+      1) this will make the message RFC2822 compliant
+      2) this will fix some problems with IMAP servers that don't accept mixed newlines
+      3) this will make easier to use regexps
+      */
+      var newData = data.replace(/\r/g, '');
+      newData = newData.replace(/\n/g, '\r\n');
+      return newData;
+    },
+
+    cleanupMozHeaders : function(aMessage) {
+      // strips off some useless headers
+      aMessage = aMessage.replace(/^From - .+\r\n/, '');
+      aMessage = aMessage.replace(/X-Mozilla-Status.+\r\n/, '');
+      aMessage = aMessage.replace(/X-Mozilla-Status2.+\r\n/, '');
+      aMessage = aMessage.replace(/X-Mozilla-Keys.+\r\n/, '');
+      return aMessage;
+    },
+
+    markAsApplied : function(aMessage) {
+      if (!Prefs.getBoolPref('extensions.hdrtoolslite.add_htl_header'))
+        return aMessage;
+
+      let now = new Date();
+      let line = 'X-ShowFirstBodyPart: applied - '+now.toString();
+      line = line.replace(/\(.+\)/, '');
+      line = line.substring(0, 75);
+      if (aMessage.indexOf('\nX-ShowFirstBodyPart: ') < 0)
+        return aMessage.replace('\r\n\r\n','\r\n'+line+'\r\n\r\n');
+      else
+        return aMessage.replace(/\nX-ShowFirstBodyPart: .+\r\n/,'\n'+line+'\r\n');
+    },
+
+    incrementDate : function(aMessage, aFolder) {
+      let isImap = aFolder.server.type == 'imap';
+      if (!isImap || !Prefs.getBoolPref('extensions.hdrtoolslite.use_imap_fix'))
+        return;
+
+      let date = this.getOrigDate(aMessage);
+      // Some IMAP provider (for ex. GMAIL) doesn't register changes in sorce if the main headers
+      // are not different from an existing message. To work around this limit, the "Date" field is
+      // modified, if necessary, adding a second to the time (or decreasing a second if second are 59)
+      let newDate = date.replace(/(\d{2}):(\d{2}):(\d{2})/, function (str, p1, p2, p3) {
+        var z = parseInt(p3) + 1;
+        if (z > 59) z = 58;
+        if (z < 10) z = '0'+z.toString();
+        return p1+':'+p2+':'+z
+      });
+      return aMessage.replace(date, newDate);
+    },
+
     // parses headers to find the original Date header, not present in nsImsgDbHdr
     getOrigDate : function(aText) {
       var dateOrig = '';
@@ -30,29 +135,19 @@
       return dateOrig;
     },
 
-    editFS: function() {
-      var msguri = gFolderDisplay.selectedMessageUris[0];
-      var mms = messenger.messageServiceFromURI(msguri).QueryInterface(Ci.nsIMsgMessageService);
-      var hdr = mms.messageURIToMsgHdr(msguri);
-      var context = {
-        hdr : hdr,
-        folder : hdr.folder
-      };
-      mms.streamMessage(msguri, new ShowFirstBodyPartStreamMessageListener(context), null, null, false, null);
+    saveToTempFile : function(aMessage) {
+      // creates the temporary file, where the modified message body will be stored
+      var tempFile = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('TmpD', Ci.nsIFile);
+      tempFile.append('HT.eml');
+      tempFile.createUnique(0, 0600);
+      var foStream = Cc['@mozilla.org/network/file-output-stream;1'].createInstance(Ci.nsIFileOutputStream);
+      foStream.init(tempFile, 2, 0x200, false); // open as "write only"
+      foStream.write(aMessage, aMessage.length);
+      foStream.close();
+      return tempFile;
     },
 
-    cleanCRLF : function(data) {
-      /* This function forces all newline as CRLF; this is useful for some reasons
-      1) this will make the message RFC2822 compliant
-      2) this will fix some problems with IMAP servers that don't accept mixed newlines
-      3) this will make easier to use regexps
-      */
-      var newData = data.replace(/\r/g, '');
-      newData = newData.replace(/\n/g, '\r\n');
-      return newData;
-    },
-
-    postActions : function(aContext) {
+    restoreState : function(aContext) {
       gDBView.selectMsgByKey(aContext.key); // select message with modified headers/source
       var hdr = aContext.folder.GetMessageHeader(aContext.key);
       if (hdr.flags & 2) {
@@ -64,11 +159,23 @@
     }
   };
 
-  // streamMessage listener
-  function ShowFirstBodyPartStreamMessageListener(aContext) {
+  // appends "hdr", "folder", and "message" to the context
+  function StreamMessageLoader(aContext) {
     this.context = aContext;
   }
-  ShowFirstBodyPartStreamMessageListener.prototype = {
+  StreamMessageLoader.prototype = {
+    load : function(aURI) {
+      var mms = messenger.messageServiceFromURI(aURI).QueryInterface(Ci.nsIMsgMessageService);
+      this.context.hdr = mms.messageURIToMsgHdr(aURI);
+      this.context.folder : this.context.hdr.folder;
+      return new Promise((aResolve, aReject) => {
+        this._resolver = aResolve;
+        this._rejector = aReject;
+        mms.streamMessage(aURI, this, null, null, false, null);
+      });
+    },
+
+    // streamMessage listener
     QueryInterface : function(iid)  {
       if (iid.equals(Ci.nsIStreamListener) ||
           iid.equals(Ci.nsISupports))
@@ -78,141 +185,109 @@
     },
 
     onStartRequest : function (aRequest, aContext) {
-      this.context.text = '';
+      this.context.message = '';
     },
 
     onStopRequest : function (aRequest, aContext, aStatusCode) {
-      var isImap = this.context.folder.server.type == 'imap';
-      var date = ShowFirstBodyPart.getOrigDate(this.context.text);
-
-      var converter = Cc['@mozilla.org/intl/scriptableunicodeconverter'].createInstance(Ci.nsIScriptableUnicodeConverter);
-      var text = this.context.text;
-      if (this.context.hdr.Charset) {
-        converter.charset = this.context.hdr.Charset;
-        // hdr.Charset will not work with multipart messages, so we must try to extract the charset manually
-      }
-      else {
-        try {
-          let textCut = text.substring(text.indexOf('charset=')+8, text.indexOf('charset=')+35);
-          let mailCharset = textCut.match(/[^\s]+/).toString();
-          mailCharset = mailCharset.replace(/\"/g, '');
-          mailCharset = mailCharset.replace(/\'/g, '');
-          converter.charset = mailCharset;
-        }
-        catch(e) {
-          converter.charset = 'UTF-8';
-        }
-      }
-      try {
-        text = converter.ConvertToUnicode(text);
-      }
-      catch(e) {}
-
-      var data = ShowFirstBodyPart.cleanCRLF(text);
-      try {
-        data = converter.ConvertFromUnicode(data);
-      }
-      catch(e) {}
-
-      // strips off some useless headers
-      data = data.replace(/^From - .+\r\n/, '');
-      data = data.replace(/X-Mozilla-Status.+\r\n/, '');
-      data = data.replace(/X-Mozilla-Status2.+\r\n/, '');
-      data = data.replace(/X-Mozilla-Keys.+\r\n/, '');
-
-      if (Prefs.getBoolPref('extensions.hdrtoolslite.add_htl_header')) {
-        let now = new Date;
-        let line = 'X-ShowFirstBodyPart: applied - '+now.toString();
-        line = line.replace(/\(.+\)/, '');
-        line = line.substring(0, 75);
-        if (data.indexOf('\nX-ShowFirstBodyPart: ') < 0)
-          data = data.replace('\r\n\r\n','\r\n'+line+'\r\n\r\n');
-        else
-          data = data.replace(/\nX-ShowFirstBodyPart: .+\r\n/,'\n'+line+'\r\n');
-      }
-
-      if (isImap && Prefs.getBoolPref('extensions.hdrtoolslite.use_imap_fix')) {
-        // Some IMAP provider (for ex. GMAIL) doesn't register changes in sorce if the main headers
-        // are not different from an existing message. To work around this limit, the "Date" field is
-        // modified, if necessary, adding a second to the time (or decreasing a second if second are 59)
-        let newDate = date.replace(/(\d{2}):(\d{2}):(\d{2})/, function (str, p1, p2, p3) {
-          var z = parseInt(p3) + 1;
-          if (z > 59) z = 58;
-          if (z < 10) z = '0'+z.toString();
-          return p1+':'+p2+':'+z
-        });
-        data = data.replace(date, newDate);
-      }
-
-      // creates the temporary file, where the modified message body will be stored
-      var tempFile = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('TmpD', Ci.nsIFile);
-      tempFile.append('HT.eml');
-      tempFile.createUnique(0, 0600);
-      var foStream = Cc['@mozilla.org/network/file-output-stream;1'].createInstance(Ci.nsIFileOutputStream);
-      foStream.init(tempFile, 2, 0x200, false); // open as "write only"
-      foStream.write(data, data.length);
-      foStream.close();
-
-      var flags = this.context.hdr.flags;
-      var keys = this.context.hdr.getStringProperty('keywords');
-
-      this.context.list = Cc['@mozilla.org/array;1'].createInstance(Ci.nsIMutableArray);
-      this.context.list.appendElement(this.context.hdr, false);
-
-      // this is interesting: nsIMsgFolder.copyFileMessage seems to have a bug on Windows, when
-      // the nsIFile has been already used by foStream (because of Windows lock system?), so we
-      // must initialize another nsIFile object, pointing to the temporary file
-      var fileSpec = Cc['@mozilla.org/file/local;1'].createInstance(Ci.nsILocalFile);
-      fileSpec.initWithPath(tempFile.path);
-      var fol = this.context.hdr.folder;
-      var extService = Cc['@mozilla.org/uriloader/external-helper-app-service;1'].getService(Ci.nsPIExternalAppLauncher)
-      extService.deleteTemporaryFileOnExit(fileSpec); // function's name says all!!!
-      this.context.noTrash = !Prefs.getBoolPref('extensions.hdrtoolslite.putOriginalInTrash');
-      let cs = Cc['@mozilla.org/messenger/messagecopyservice;1'].getService(Ci.nsIMsgCopyService);
-      cs.CopyFileMessage(fileSpec, fol, null, false, flags, keys, new ShowFirstBodyPartCopyListener(this.context), msgWindow);
+      this._resolver(this.context);
     },
 
     onDataAvailable : function (aRequest, aContext, aInputStream, aOffset, aCount) {
       var scriptStream = Cc['@mozilla.org/scriptableinputstream;1'].createInstance().QueryInterface(Ci.nsIScriptableInputStream);
       scriptStream.init(aInputStream);
-      this.context.text += scriptStream.read(scriptStream.available());
+      this.context.message += scriptStream.read(scriptStream.available());
     }
   };
 
-  // copyFileMessage listener
-  function ShowFirstBodyPartCopyListener(aContext) {
+  function MessageReplacer(aContext) {
     this.context = aContext;
   }
-  ShowFirstBodyPartCopyListener.prototype = {
+  MessageReplacer.prototype = {
+    replaceFromFile : function(aFile) {
+      let flags = this.context.hdr.flags;
+      let keys = this.context.hdr.getStringProperty('keywords');
+
+      // this is interesting: nsIMsgFolder.copyFileMessage seems to have a bug on Windows, when
+      // the nsIFile has been already used by foStream (because of Windows lock system?), so we
+      // must initialize another nsIFile object, pointing to the temporary file
+      let fileSpec = Cc['@mozilla.org/file/local;1'].createInstance(Ci.nsILocalFile);
+      fileSpec.initWithPath(aFile.path);
+
+      let extService = Cc['@mozilla.org/uriloader/external-helper-app-service;1'].getService(Ci.nsPIExternalAppLauncher)
+      extService.deleteTemporaryFileOnExit(fileSpec); // function's name says all!!!
+
+      return new Promise((aResolve, aReject) => {
+        this._resolver = aResolve;
+        this._rejector = aReject;
+        this._replaced = false;
+        this._readyToFinish = false;
+        let cs = Cc['@mozilla.org/messenger/messagecopyservice;1'].getService(Ci.nsIMsgCopyService);
+        cs.CopyFileMessage(fileSpec, this.context.hdr.folder, null, false, flags, keys, this, msgWindow);
+      });
+    },
+
+    tryResolve : function() {
+      if (!this._replaced || !this._readyToFinish)
+        return;
+      this._resolver(this.context);
+    },
+
+    // copyFileMessage listener
     QueryInterface : function(iid) {
       if (iid.equals(Ci.nsIMsgCopyServiceListener) ||
-          iid.equals(Ci, true, null, false))
+          iid.equals(Components.interfaces.nsISupports))
         return this;
 
       throw Components.results.NS_NOINTERFACE;
+    },
+    GetMessageId: function (messageId) {},
+    OnProgress: function (progress, progressMax) {},
+    OnStartCopy: function () {},
+    OnStopCopy: function (status) {
+      if (status == 0) { // copy done
+        let hdrs = Cc['@mozilla.org/array;1'].createInstance(Ci.nsIMutableArray);
+        hdrs.appendElement(this.context.hdr, false);
+        let noTrash = !Prefs.getBoolPref('extensions.hdrtoolslite.putOriginalInTrash');
+        this.context.folder.deleteMessages(hdrs, null, noTrash, true, null, false);
+
+        this._replaced = true;
+        this.tryResolve();
+      }
     },
     SetMessageKey: function (key) {
       this.context.key = key;
       // at this point, the message is already stored in local folders, but not yet in remote folders,
       // so for remote folders we use a folderListener
       if (this.context.folder.server.type == 'imap' || this.context.folder.server.type == 'news') {
-        let folderListener = new ShowFirstBodyPartFolderListener(this.context)
-        this.context.URI = this.context.folder.URI;
-        Cc['@mozilla.org/messenger/services/session;1'].getService(Ci.nsIMsgMailSession).AddFolderListener(folderListener, Ci.nsIFolderListener.all);
+        let watcher = new RemoteFolderWatcher(this.context);
+        watcher.waitUntilAdded().then(() => {
+          this._readyToFinish = true;
+          this.tryResolve();
+        });
       }
       else {
-        setTimeout(function() {
-          ShowFirstBodyPart.postActions(this.context);
+        setTimeout(() => {
+          this._readyToFinish = true;
+          this.tryResolve();
         }, 500);
       }
     }
   };
 
   // used just for remote folders
-  function ShowFirstBodyPartFolderListener(aContext) {
+  function RemoteFolderWatcher(aContext) {
     this.context = aContext;
   }
-  ShowFirstBodyPartFolderListener.prototype = {
+  RemoteFolderWatcher.prototype = {
+    waitUntilAdded : function() {
+      return new Promise((aResolve, aReject) {
+        this._resolver = aResolve;
+        this._rejector = aReject;
+        Cc['@mozilla.org/messenger/services/session;1'].getService(Ci.nsIMsgMailSession).AddFolderListener(this, Ci.nsIFolderListener.all);
+      });
+    },
+
+    // folder listener
     OnItemAdded: function(parentItem, item, view) {
       try {
         var hdr = item.QueryInterface(Ci.nsIMsgDBHdr);
@@ -220,8 +295,9 @@
       catch(e) {
         return;
       }
-      if (this.key == hdr.messageKey && this.URI == hdr.folder.URI) {
-        ShowFirstBodyPart.postActions(this.context);
+      if (this.context.key == hdr.messageKey &&
+          this.context.folder.URI == hdr.folder.URI) {
+        this._resolver(this.context);
         // we don't need anymore the folderListener
         Cc['@mozilla.org/messenger/services/session;1'].getService(Ci.nsIMsgMailSession).RemoveFolderListener(this);
       }
